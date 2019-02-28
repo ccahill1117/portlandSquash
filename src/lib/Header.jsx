93ba024f899db91f33a557c0a4382abf0aee734c
Student <example@example.com>
2019-02-28 14:08:39 -0800

    initial commit

---
+++ src/lib/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header(){
+  return(
+    <div>
+      <h2>Header</h2>
+    </div>
+  )
+
+}
+
+export default Header;
