75e0ad4c2fc0235bd60f5b57e8c36acb414454c0
Christopher Cahill <cthomascahill@gmail.com>
2019-03-09 23:09:01 -0800

    add reducers and begin to create store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 
 ReactDOM.render(
 
