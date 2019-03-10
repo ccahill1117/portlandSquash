dce1999225977b554ac0d41c8adf078aa238f864
Christopher Cahill <cthomascahill@gmail.com>
2019-03-10 13:59:52 -0700

    okta sign in widget added"

--- src/lib/UserSignIn.jsx
+++ src/lib/UserSignIn.jsx
@@ -2,51 +2,65 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+var Backbone = require('backbone');
 
 
-function UserSignIn(){
-  var signInStyle = {
-    borderStyle : 'solid',
-    margin: '10px',
-    padding: '10px',
+export default class UserSignIn extends React.Component{
+  constructor(){
+    super();
+    this.state = { user: null };
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://https://dev-816756.okta.com',
+      clientId: '[0oabr696vWGka8xg6356]',
+      redirectUri: 'http://localhost:8080/implicit/callback',
+      authParams: {
+        responseType: 'id_token'
+      }
+    });
+    this.showLogin = this.showLogin.bind(this);
+    this.logout = this.logout.bind(this);
   }
-
-  var OktaSignIn = require('@okta/okta-signin-widget');
-
-  var signIn = new OktaSignIn(
-    {
-      baseUrl: 'https://{https://dev-816756.okta.com}',
-      el: '#osw-container'
-    }
-  );
-
-  signIn.showSignInToGetTokens({
-    clientId: '{{0oabr696vWGka8xg6356}}',
-    redirectUri: '{{http://localhost:8080/implicit/callback}}',
-
-    // Return an access token from the authorization server
-    getAccessToken: true,
-
-    // Return an ID token from the authorization server
-    getIdToken: true,
-
-    scope: 'openid profile',
-
-  });
-
-
-
-  return (
-    <div style={signInStyle}>
-
-      <div id="osw-container">
-
+  componentDidMount(){
+    this.widget.session.get((response) => {
+      if(response.status !== 'INACTIVE'){
+        this.setState({user:response.login});
+      }else{
+        this.showLogin();
+      }
+    });
+  }
+  showLogin(){
+    Backbone.history.stop();
+    this.widget.renderEl({el:this.loginContainer},
+      (response) => {
+        this.setState({user: response.claims.email});
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  logout(){
+    this.widget.signOut(() => {
+      this.setState({user: null});
+      this.showLogin();
+    });
+  }
+  render(){
+    return(
+      <div>
+        {this.state.user ? (
+          <div className="container">
+            <div>Welcome, {this.state.user}!</div>
+            <button onClick={this.logout}>Logout</button>
+          </div>
+        ) : null}
+        {this.state.user ? null : (
+          <div ref={(div) => {this.loginContainer = div; }} />
+        )}
       </div>
-
-
-    </div>
-  )
-
+    );
+  }
 }
-
-export default UserSignIn;
