4176164419b96c89550addbbb2170dfe5b3a6b39
Christopher Cahill <cthomascahill@gmail.com>
2019-03-09 23:14:21 -0800

    build out more project directory

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+import constants from './../constants';
+const { initialState, types } = constants;
+
+const userReducer = (state = initialState.currentUser, action) => {
+
+
+    return state;
+  }
+
+
+
+export default userReducer;
