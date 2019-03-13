65b55e87bc5185533e08896858afd51bb95e7df1
Student <example@example.com>
2019-03-13 15:23:16 -0700

    console log fetch

--- PCSL/config/application.rb
+++ PCSL/config/application.rb
@@ -20,12 +20,20 @@ Bundler.require(*Rails.groups)
 module PCSL
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
 
-config.middleware.use Rack::MethodOverride
-config.middleware.use ActionDispatch::Flash
-config.middleware.use ActionDispatch::Cookies
-config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
+
+    config.load_defaults 5.2
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,83 +1,45 @@
 import React, { Component } from 'react'
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
-class App extends Component {
+class Players extends Component {
   constructor () {
     super()
-    this.state = {}
-    this.getPlayers = this.getPlayers.bind(this)
-    this.getPlayer = this.getPlayer.bind(this)
+    this.state = {
+      players: [],
+    };
+
   }
 
-  componentDidMount () {
-    this.getPlayers()
-  }
-
-  fetch (endpoint) {
-    return window.fetch(endpoint)
-      .then(response => response.json())
-      .catch(error => console.log(error))
-  }
-
-  getPlayers () {
-    this.fetch('/api/players')
-      .then(players => {
-        if (players.length) {
-          this.setState({players: players})
-          this.getPlayer(players[0].id)
-        } else {
-          this.setState({players: []})
-        }
+  componentDidMount() {
+    fetch('http://localhost:3001/api/players')
+      .then(function(response) {
+        return response.json();
       })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
   }
 
-  getPlayer (id) {
-    this.fetch(`/api/players/${id}`)
-      .then(player => this.setState({player: player}))
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   render () {
-    let {players, player} = this.state
-    return players
-      ? <Container text>
-        <Header as='h2' icon textAlign='center' color='teal'>
-          <Icon name='unordered list' circular />
-          <Header.Content>
-            List of Ingredients
-          </Header.Content>
-        </Header>
-        <Divider hidden section />
-        {players && players.length
-          ? <Button.Group color='teal' fluid widths={players.length}>
-            {Object.keys(players).map((key) => {
-              return <Button active={player && player.id === players[key].id} fluid key={key} onClick={() => this.getPlayer(players[key].id)}>
-                {players[key].title}
-              </Button>
-            })}
-          </Button.Group>
-          : <Container textAlign='center'>No players found.</Container>
-        }
-        <Divider section />
-        {player &&
-          <Container>
-            <Header as='h2'>{player.title}</Header>
-            {player.description && <p>{player.description}</p>}
-            {player.ingredients &&
-              <Segment.Group>
-                {player.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
-              </Segment.Group>
-            }
-            {player.steps && <p>{player.steps}</p>}
-            {player.source && <Button basic size='tiny' color='teal' href={player.source}>Source</Button>}
-          </Container>
-        }
-      </Container>
-      : <Container text>
-        <Dimmer active inverted>
-          <Loader content='Loading' />
-        </Dimmer>
-      </Container>
+    return (
+
+    <div></div>
+  )
   }
 }
 
-export default App
+export default Players
