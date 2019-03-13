398c51a949f84e255459258a0c7910e99fa4adf3
Student <example@example.com>
2019-03-12 17:20:51 -0700

    close

--- PCSL_backend/app/models/team.rb
+++ PCSL_backend/app/models/team.rb
@@ -1,6 +1,3 @@
 class Team < ApplicationRecord
-  has_many :players
-
-  belongs_to :player_id_1, :class_name => 'Player', :foreign_key => 'player_id_1'
-
+  has_many :players, class_name: "Player", foreign_key: 'player_id'
 end
